6b36f49ca5e639a88313835c5e2d48dec7bc38be
ArthurFontis <arthurfontis17@gmail.com>
2026-01-27 23:39:34 -0300

    27/01/26 - front end startado

---
+++ frontend/src/config/store.js
@@ -0,0 +1,25 @@
+import Vue from "vue"
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        isMenuVisible: true,
+        user: {
+            name: 'Usuário Mock',
+            email: 'mock@ex.com.br'
+        }
+    },
+    mutations: {
+        toggleMenu(state, isVisible) {
+            if(isVisible === undefined) {
+                state.isMenuVisible = !state.isMenuVisible
+            } else {
+                state.isMenuVisible = isVisible
+            }
+
+            console.log('toggleMenu = ' + state.isMenuVisible)
+        }
+    }
+})
