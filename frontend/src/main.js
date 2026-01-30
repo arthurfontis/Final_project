be69352f82c14e9b409e188dca6498dec1343307
ArthurFontis <arthurfontis17@gmail.com>
2026-01-29 22:32:43 -0300

    29/01/26 - frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,13 +4,14 @@ import Vue from 'vue'
 import App from './App'
 
 import './config/bootstrap'
+import './config/msgs'
 import store from './config/store'
 import router from './config/router'
 
 Vue.config.productionTip = false
 
 // TEMPORÁRIO !!!
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFydGh1ciBDLiBGb250aXMiLCJlbWFpbCI6ImZvbnRpc0BleC5jb20uYnIiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNzY5NjUwNTc5LCJleHAiOjE3Njk5MDk3Nzl9.sM5k0w8IqgJpFDCZaWz80lHw72vfGp2EOO5qKH4AsxI'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFydGh1ciBDLiBGb250aXMiLCJlbWFpbCI6ImZvbnRpc0BleC5jb20uYnIiLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNzY5NzI1NjEyLCJleHAiOjE3Njk5ODQ4MTJ9.8CgW9GFtrr1d09d9HOps-ejkjF1dW4r6vIbJrB2gWvs'
 
 new Vue({
   store,
